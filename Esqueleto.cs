bc09418900761c6e8f6b0d2941cd2edd7cd824cc
Raphaela <bmraphaela@gmail.com>
2025-01-31 16:23:21 -0300

    :sparkles: feat: adicionando estrutura c#

---
+++ Esqueleto.cs
@@ -0,0 +1,31 @@
+using System;
+
+Console.WriteLine("Hello world!");
+
+namespace YourNamespace
+{
+    class YourClass
+    {
+    }
+
+    struct YourStruct
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+        }
+    }
+}
